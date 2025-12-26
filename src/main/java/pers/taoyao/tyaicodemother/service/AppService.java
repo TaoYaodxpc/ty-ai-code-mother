cc00f556abf1774d77a93bcf94d0279f6ed00ed1
TaoYao <admin@123>
2025-12-26 16:13:13 +0800

    第四期 应用模块 - 2: 使用 AI 生成应用模块 CRUD

--- src/main/java/pers/taoyao/tyaicodemother/service/AppService.java
+++ src/main/java/pers/taoyao/tyaicodemother/service/AppService.java
@@ -1,7 +1,12 @@
 package pers.taoyao.tyaicodemother.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import pers.taoyao.tyaicodemother.model.dto.app.AppQueryRequest;
 import pers.taoyao.tyaicodemother.model.entity.App;
+import pers.taoyao.tyaicodemother.model.vo.AppVO;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,27 @@ import pers.taoyao.tyaicodemother.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用视图对象
+     *
+     * @param app 应用实体对象
+     * @return 应用视图对象
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取查询条件
+     *
+     * @param appQueryRequest 应用查询请求
+     * @return 查询条件
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * 获取应用视图对象列表
+     *
+     * @param appList 应用实体对象列表
+     * @return 应用视图对象列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
 }
