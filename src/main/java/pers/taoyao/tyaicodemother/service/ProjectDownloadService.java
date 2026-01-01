bc200f19aafdfea9cd8ce7912c3a5f512f99c1c7
TaoYao <admin@123>
2026-01-01 17:02:50 +0800

    第七期 功能扩展 - 2（提供代码下载功能）

---
+++ src/main/java/pers/taoyao/tyaicodemother/service/ProjectDownloadService.java
@@ -0,0 +1,24 @@
+package pers.taoyao.tyaicodemother.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import pers.taoyao.tyaicodemother.model.entity.User;
+
+/**
+ *
+ * @author admin
+ * @date 2026/1/1
+ * @Version v1.0
+ * @description
+ */
+public interface ProjectDownloadService {
+
+    /**
+     * 下载项目为 zip
+     *
+     * @param projectPath 项目路径
+     * @param downloadFileName 下载文件名
+     * @param response 响应
+     * @return 下载结果
+     */
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
